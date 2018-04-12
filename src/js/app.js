1b3c5ecb9f6e222f58f1832307f05aa167f8090b
Flavio Coutinho <fegemo@gmail.com>
2018-04-12 16:37:18 -0300

    Initial commit :palm_tree:

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+require('../css/main.css');
+
+
+
+
+
+
+
+
+
+const metaphorContainerEl = document.querySelector('#metaphor-container');
+const metaphorModelviewEl = document.querySelector('#metaphor-modelview');
+
+metaphorContainerEl.innerHTML = '';
+metaphorContainerEl.appendChild(metaphorModelviewEl);
